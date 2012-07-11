0d2c5dbe3152fb705df044cc99c7ae5a5090a83b
James Wanga <james@thedigitalself.com>
2012-07-11 12:19:36 -0400

    Added server.js

---
+++ server.js
@@ -0,0 +1,67 @@
+/*
+NodeJS Static Http Server - http://github.com/thedigitalself/node-static-http-server/
+By James Wanga - The Digital Self
+Licensed under a Creative Commons Attribution 3.0 Unported License.
+
+A simple, nodeJS, http development server that trivializes serving static files.
+
+This server is HEAVILY based on work done by Ryan Florence(https://github.com/rpflorence) (https://gist.github.com/701407). I merged this code with suggestions on handling varied MIME typed found at Stackoverflow (http://stackoverflow.com/questions/7268033/basic-static-file-server-in-nodejs).
+
+To run the server simply place the server.js file in the root of your web application and issue the command 
+$ node server.js 
+or 
+$ node server.js 1234 
+with "1234" being a custom port number"
+
+Your web application will be served at http://localhost:8888 by default or http://localhost:1234 with "1234" being the custom port you passed.
+
+*/
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+    port = process.argv[2] || 8888;
+
+var mimeTypes = {
+    "htm": "text/html",
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "gif": "image/gif",
+    "js": "text/javascript",
+    "css": "text/css"};
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+  
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+	if (fs.statSync(filename).isDirectory()) filename += '/mockup.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+      response.writeHead(200, {"Content-Type": mimeType});
+      response.write(file, "binary");
+      console.log('rendered ' + filename + ' as ' + mimeType);
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
